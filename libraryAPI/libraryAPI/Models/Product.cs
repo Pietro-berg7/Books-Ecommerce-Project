cdd8a052d637872e59eb57c76684c693d5c81548
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-08-28 18:51:51 -0300

    Iniciando Back-end e criando a classe Produto

---
+++ libraryAPI/libraryAPI/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace libraryAPI.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+        public string Img { get; set; }
+    }
+}
