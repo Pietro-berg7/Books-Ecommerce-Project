a07c3773c24b0931f6e24b8e1398b6474ef000f5
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-08-28 21:00:56 -0300

    Criando a classe ToDoContext

---
+++ libraryAPI/libraryAPI/Models/ToDoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> option)
+            : base(option)
+        { }
+
+        public DbSet<Product> todoProducts { get; set; }
+    }
+}
